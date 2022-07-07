fee2bd84b6f32914f95503231d73da5d405c7c56
Caroline de Camargo Flach <caroline.camargo@squadra.com.br>
2022-07-07 14:57:38 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/caroline/hrpayroll/controller/PaymentController.java
+++ hr-payroll/src/main/java/com/caroline/hrpayroll/controller/PaymentController.java
@@ -2,6 +2,7 @@ package com.caroline.hrpayroll.controller;
 
 import com.caroline.hrpayroll.entity.Payment;
 import com.caroline.hrpayroll.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +17,13 @@ public class PaymentController {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable long workerId, @PathVariable Integer days){
         return ResponseEntity.ok(service.getPayment(workerId, days));
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(long workerId, Integer days){
+        return ResponseEntity.ok(new Payment("Brann", 400.0, days));
     }
 }
--- hr-worker/src/main/java/com/caroline/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/caroline/hrworker/controller/WorkerController.java
@@ -3,7 +3,6 @@ package com.caroline.hrworker.controller;
 import com.caroline.hrworker.entity.Worker;
 import com.caroline.hrworker.repository.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,9 +14,6 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerController {
-
-    @Autowired
-    private Environment environment;
 
     @Autowired
     private WorkerRepository repository;
