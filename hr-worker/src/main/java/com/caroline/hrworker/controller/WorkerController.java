dd7276f7b75c39e5520082a60eb02bdd54ea7701
Caroline de Camargo Flach <caroline.camargo@squadra.com.br>
2022-07-07 10:56:03 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/caroline/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/caroline/hrworker/controller/WorkerController.java
@@ -2,7 +2,10 @@ package com.caroline.hrworker.controller;
 
 import com.caroline.hrworker.entity.Worker;
 import com.caroline.hrworker.repository.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,11 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerController {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+    @Autowired
+    private Environment environment;
+
     @Autowired
     private WorkerRepository repository;
 
@@ -25,6 +33,9 @@ public class WorkerController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> getById(@PathVariable Long id){
+
+        logger.info("PORT = " + environment.getProperty("local.server.port"));
+
         return ResponseEntity.ok(repository.findById(id).get());
     }
 }
