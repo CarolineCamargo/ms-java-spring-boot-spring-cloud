d9de25d15fcf059f30e5ea11a64f64e90ab2b5f7
Caroline de Camargo Flach <caroline.camargo@squadra.com.br>
2022-07-08 10:20:04 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/caroline/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/caroline/hrworker/controller/WorkerController.java
@@ -2,7 +2,10 @@ package com.caroline.hrworker.controller;
 
 import com.caroline.hrworker.entity.Worker;
 import com.caroline.hrworker.repository.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,11 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerController {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private WorkerRepository repository;
 
@@ -26,5 +34,11 @@ public class WorkerController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> getById(@PathVariable Long id){
         return ResponseEntity.ok(repository.findById(id).get());
+    }
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs(){
+        logger.info("CONFIG= " + testConfig);
+        return ResponseEntity.noContent().build();
     }
 }
